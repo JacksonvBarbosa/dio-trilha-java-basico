e9a0636860dfc004fd533addd18747f7e8f43c4b
Jackson Ventura <jacksonvbarbosa@terra.com.br>
2023-08-27 03:01:52 -0300

    adicionando pacote método

---
+++ tipos-variaveis/src/aula/tiposmetodos/Metodos.java
@@ -0,0 +1,18 @@
+package aula.tiposmetodos;
+
+public class Metodos {
+        //Não é utilizado o método main nesse caso
+        //Métodos é um ação e por padrão usamos camelCase inicio minusculo segundo inicio maiúsculo
+
+        public double somar(int num5, int num4){
+            return num5 + num4;
+        }
+
+        public void imprimir(String texto){
+            //Não necessita de retorno (return)
+        }
+
+        //public double dividir(int dividendo, int divisor) throws Exception{} {
+            //return;
+        //}
+}
