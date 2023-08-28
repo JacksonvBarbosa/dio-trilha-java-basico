adedc8dc9e1878f9691566b880b7baa20cf447fe
Jackson Ventura <jacksonvbarbosa@terra.com.br>
2023-08-28 03:40:17 -0300

    Desafio concluido e testado

--- dio-desafios/ContaBanco/src/ContaTerminal.java
+++ dio-desafios/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double saldo = 2589.00;
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int conta = entrada.nextInt();
+        System.out.println("Por favor, digite o número da Agência: ");
+        int agencia = entrada.nextInt();
+        System.out.println("Por favor, digite seu Nome: ");
+        String nome = entrada.next();
+
+        System.out.println("Olá ".concat(nome) + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " ,conta " + conta + " e seu saldo R$" + saldo + " já está disponível para saque.");
+
     }
 }
